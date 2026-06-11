742f9998fe08736e7c07094823d48766fbbd9f26
shin.maruyama <s.maruyama@allin-one.co.jp>
2026-06-11 10:41:45 +0900

    Add UserSelectStringAssignment plugin

---
+++ plugins/UserSelectStringAssignment/UserSelectStringAssignment/contents/js/config.js
@@ -0,0 +1,491 @@
+// Copyright (C) All in one Allright Reserved.
+
+jQuery.noConflict();
+
+(async function ($, PLUGIN_ID) {
+  'use strict';
+
+  //[処理用オブジェクト]
+  const obj = {
+    /**
+     * @param $submit [保存ボタン要素]
+     * @param $cancel [キャンセルボタン要素]
+     * @param config  [プラグイン設定内容オブジェクト]
+     */
+    $submit: $('#submit'),
+    $cancel: $('#cancel'),
+    config: kintone.plugin.app.getConfig(PLUGIN_ID),
+    fieldList: [],
+
+    /********************************************
+     * [プラグイン設定保存処理関数]
+     * @returns [プラグイン保存設定内容オブジェクト]
+     ********************************************/
+    submit: function () {
+      const value = {
+        settings: [],
+      };
+
+      for (let i = 0; i < $('.main-contents').length; i++) {
+        const $mainContent = $('.main-contents').eq(i);
+
+        const item = {
+          delimiter: $mainContent.find('.delimiter').val(),
+          source: $mainContent.find('.source-select').val() ? $mainContent.find('.source-select').val() : 'none',
+          destination: $mainContent.find('.destination-select').val() ? $mainContent.find('.destination-select').val() : 'none',
+        };
+        value.settings.push(item);
+      }
+      return value;
+    },
+
+    /**********************************************************
+     * [プラグイン設定画面表示時処理関数]
+     * @param {object} config [プラグイン保存設定内容オブジェクト]
+     **********************************************************/
+    configShow: async function (config) {
+
+
+      if (!(obj.checkCertificationFile())) {
+        obj.displayAlert('エラー', '不正ファイルのため<br>ご利用できません。', 'error', 'OK');
+        return;
+      } else {
+        if (!(await KNTP283810certification())) {
+          return;
+        }
+      }
+      /**
+       * [保存・削除ボタンクリック時の処理]
+       * [追加・削除ボタンクリック時の処理]
+       */
+      obj.buttonClickEvent();
+      obj.rowButtonClickEvent();
+      obj.changeEvent();
+
+      /**
+       * @param {Array} fieldList [フォームの左上から順のフィールドリスト]
+       */
+      obj.fieldList = await obj.getFieldList(true);
+      const sourceFieldList = obj.filterField(obj.fieldList, true, 'USER_SELECT');
+      const destinationFieldList = obj.filterField(obj.fieldList, true, 'SINGLE_LINE_TEXT');
+
+      //[ドロップダウンにオプション追加]
+      obj.createOption(sourceFieldList, $('.source-select'));
+      obj.createOption(destinationFieldList, $('.destination-select'));
+
+      //[既にプラグイン設定が保存されている場合の処理]
+      if (Object.keys(config).length) {
+        //[JSON型に変換]
+        config.settings = JSON.parse(config.settings);
+        const { settings } = config;
+        // console.log(settings);
+
+        //[保存した設定個数分クローン作成 *1つ目は既にあるので飛ばす]
+        for (let i = 1; i < settings.length; i++) {
+          const $clone = $('.main-contents:first').clone(true);
+          $('#parent').append($clone);
+        }
+
+        //[プラグイン保存設定反映]
+        for (let i = 0; i < settings.length; i++) {
+          const $mainContent = $('.main-contents').eq(i);
+          $mainContent.find('.delimiter').val(settings[i].delimiter);
+          $mainContent.find('.source-select').val(settings[i].source);
+          $mainContent.find('.destination-select').val(settings[i].destination);
+        }
+
+        //obj.search();
+        await obj.createNewOption();
+        obj.adjustDis();
+      } else {
+        obj.search();
+      }
+
+      $('#parent').sortable();
+    },
+
+    changeEvent: function () {
+      $(document).on('change', '.source-select,.destination-select', async function () {
+        var scrollPositionY = window.scrollY;
+        await obj.createNewOption();
+        obj.adjustDis();
+        window.scrollTo(0, scrollPositionY);
+      });
+    },
+
+    /*************************************
+     * [設定追加・削除ボタンクリック時の処理関数]
+     ************************************/
+    rowButtonClickEvent: function () {
+      //[追加ボタンクリック時の処理]
+      $(document).on('click', '.add-row', async function () {
+        $('.source-select').select2('destroy');
+        $('.destination-select').select2('destroy');
+
+        const $clone = $('.main-contents:first').clone(true);
+        $clone.find('.delimiter').val('');
+        $(this).closest('.main-contents').after($clone);
+
+        //obj.search();
+        await obj.createNewOption();
+        obj.adjustDis();
+      });
+
+      //[削除ボタンクリック時の処理]
+      $(document).on('click', '.remove-row', async function () {
+        if ($('.main-contents').length > 1) $(this).closest('.main-contents').remove();
+        await obj.createNewOption();
+        obj.adjustDis();
+      });
+    },
+
+    adjustDis: function () {
+      obj.search();
+
+      for (let i = 0; i < $('.main-contents').length; i++) {
+        const $mainContent = $('.main-contents').eq(i);
+        let str1 = $mainContent.find('.source-select option:selected').text().length;
+        const str2 = $mainContent.find('.destination-select option:selected').text().length;
+
+        if (str2 > str1) str1 = str2;
+        let len = str1 * 17 + 20;
+        if (len < 290) len = 290;
+
+        $mainContent.find('.container').find('.select2-selection--single').css('width', len + 'px');
+        $mainContent.find('.container').find('.select2-selection__arrow').css('left', len - 30 + 'px');
+        $mainContent.css('width', len + 50 + 'px');
+      };
+    },
+
+    createNewOption: async function () {
+      let array = [];
+
+      $('.main-contents').each(function () {
+        array.push($(this).find('.source-select').val());
+        array.push($(this).find('.destination-select').val());
+      });
+
+      const sourceFieldList = obj.filterField(obj.fieldList, true, 'USER_SELECT');
+      const destinationFieldList = obj.filterField(obj.fieldList, true, 'SINGLE_LINE_TEXT');
+
+      $('.main-contents').each(function () {
+        const val1 = $(this).find('.source-select').val();
+        const val2 = $(this).find('.destination-select').val();
+
+        //反映元は何度も選択する可能性があるためフィルターしない
+        //const newOptions1 = sourceFieldList.filter(x => !array.includes(x.code) || x.code === val1);
+        $(this).find('.source-select').empty();
+        obj.createOption(sourceFieldList, $(this).find('.source-select'));
+        $(this).find('.source-select').val(val1);
+
+        const newOptions2 = destinationFieldList.filter(x => !array.includes(x.code) || x.code === val2);
+        $(this).find('.destination-select').empty();
+        obj.createOption(newOptions2, $(this).find('.destination-select'));
+        $(this).find('.destination-select').val(val2);
+      });
+
+    },
+
+    /*************************************************
+     * [自アプリフォームのフィールドリストを取得する]
+     * @param {boolean} subTable [サブテーブル内も取得する場合true しない場合false]
+     * @returns [自アプリフォームの左上から順番にフィールド取得]
+     ************************************************/
+    getFieldList: async function (subTable = false) {
+      const fieldList = [];
+      const fieldList2 = [
+        ...Object.values(cybozu.data.page.FORM_DATA.schema.table.fieldList),
+        ...Object.values(cybozu.data.page.FORM_DATA.schema.subTable),
+      ];
+
+      try {
+        const resp = await kintone.api(kintone.api.url('/k/v1/app/form/layout.json', true), 'GET', {
+          app: kintone.app.getId(),
+        });
+        resp.layout.forEach((row) => {
+          if (row.type === 'ROW') row.fields.forEach((field) => fieldList.push(field));
+          else if (row.type === 'SUBTABLE') {
+            fieldList.push(row);
+            if (!subTable) return;
+            row.fields.forEach((field) => {
+              const fieldInfo = {
+                type: field.type,
+                code: `${row.code}　${field.code}`,
+                id: `${fieldList2.find((x) => x.var === row.code).id}　${Object.values(fieldList2.find((x) => x.var === row.code).fieldList).find((y) => y.var === field.code).id
+                  }`,
+                properties: Object.values(fieldList2.find((x) => x.var === row.code).fieldList).find(
+                  (y) => y.var === field.code
+                ).properties,
+              };
+              fieldList.push(fieldInfo);
+            });
+          } else if (row.type === 'GROUP') {
+            fieldList.push(row);
+            row.layout.forEach((childRow) => childRow.fields.forEach((field) => {
+              fieldList.push({
+                type: field.type,
+                code: row.code + '　' + field.code,
+                id: field.id,
+                properties: field.properties,
+              })
+            }
+              //fieldList.push(field)
+            ));
+          }
+        });
+      } catch { }
+
+      fieldList.forEach((field) => {
+        const target = fieldList2.find((x) => x.var === field.code);
+        if (!target) return;
+        field.id = target.id;
+        field.properties = target.properties;
+        field.label = target.label;
+
+        if (!(field.type === 'SUBTABLE' && field.id === target.id)) return;
+        field.fields.forEach((inField) => {
+          const inTarget = Object.values(target.fieldList).find((x) => x.var === inField.code);
+          inField.id = inTarget.id;
+          inField.properties = inTarget.properties;
+          inField.label = inTarget.label;
+        });
+      });
+      return fieldList;
+    },
+
+    /************************************************
+     * [指定したフィールドを抽出する関数]
+     * @param {Array}   fieldList      [フィルターをかけるフィールドリスト]
+     * @param {boolean} flg            [指定したフィールドタイプを抽出 true   以外を抽出 false]
+     * @param {Array}   limitFieldType [抽出するフィールドタイプリスト]
+     * @returns [第二引数がtrueなら指定したフィールドタイプのフィールドリスト
+     *                    falseなら指定したフィールドタイプ以外のフィールドリスト]
+     ************************************************/
+    filterField: function (fieldList, flg, ...limitFieldType) {
+      if (!limitFieldType.length) return fieldList;
+      if (flg) return fieldList.filter((x) => limitFieldType.includes(x.type));
+      else return fieldList.filter((x) => !limitFieldType.includes(x.type));
+    },
+
+    /***************************************
+     * [ドロップダウンに検索機能追加・CSS追加]
+     **************************************/
+    search: function () {
+      $('.select2').select2({
+        //width: '300px',
+      }).on('select2:open', function (e) {
+        setTimeout(function () {
+          var optionCount = $('.select2-results__option').length;
+          if (optionCount > 5) {
+            var newTop = 0;
+          } else {
+            var newTop = -40;
+          }
+
+          $('.select2-search__field').css({
+            height: '34px',
+            'width': "280px",
+          });
+
+          $('.select2-search--dropdown').css({
+            padding: '2px',
+          });
+
+          $('.select2-dropdown--below').css({
+            'min-width': '290px',
+            width: 'auto !important',
+          });
+          $('.select2-dropdown--above').css({
+            'min-width': '290px',
+            width: 'auto !important',
+            'top': newTop + 'px',
+          });
+          $('.select2-results__option').css({
+            'min-width': '290px',
+            width: 'auto !important',
+            height: '30px',
+            padding: '4px 0px',
+            'vartical-align': 'center',
+            'white-space': 'nowrap',
+            'overflow': 'visible',
+          });
+        }, 0);
+      });
+
+      $('.select2-selection--single').css({
+        width: '290px',
+        height: '55px',
+        border: '1px solid #e3e7e8',
+        'background-color': '#f7f9fa',
+        'box-shadow': '1px 1px 1px #fff inset',
+        'border-radius': '0',
+        'text-overflow': 'ellipsis',
+        'margin-top': '-3px',
+      });
+
+      $('.select2-selection__rendered').css({
+        color: '#34a3db',
+        'text-align': 'center',
+        'line-height': '55px',
+      });
+
+      $('.select2-selection__arrow').css({
+        top: '11px',
+        left: '260px',
+      });
+
+      $('.select2-container--default .select2-selection--single .select2-selection__arrow b').css({
+        'border-color': '#3498db transparent transparent transparent',
+      });
+    },
+
+    /***********************************************
+     * [オプション作成処理関数]
+     * @param {Array}  fields [フィールドリスト]
+     * @param {jQuery} name   [クラス・ID名]
+     ***********************************************/
+    createOption: function (fields, name) {
+      const $noneOption = $('<option>', {
+        value: 'none',
+        text: '',
+      });
+      name.append($noneOption);
+
+      fields.forEach((field) => {
+        if (field.type !== 'SPACER') {
+          const $option = $('<option>', {
+            value: field.code,
+            text: field.code,
+          });
+          name.append($option);
+        } else {
+          const $option = $('<option>', {
+            value: field.elementId,
+            text: field.elementId,
+          });
+          name.append($option);
+        }
+      });
+    },
+
+    /******************************************
+     * [アラートの表示処理関数]
+     * @param {string} title  [タイトル]
+     * @param {string} text   [説明文]
+     * @param {string} type   [アラートタイプ]
+     * @param {string} button [ボタン名表示文字]
+     * 使用例 ： obj.displayAlert('エラー','選択されたフィールドが重複しています。','error','OK');
+     *****************************************/
+    displayAlert: function (title, text, type, button) {
+      swal.fire({
+        title: title,
+        html: text,
+        icon: type,
+        confirmButtonText: button,
+      });
+    },
+
+    /***********************************************************
+     * [必須チェック処理関数]
+     * @param {array} value [保存設定内容]
+     * @returns [抜けがある場合 true　ない場合 falseを返す]
+     **********************************************************/
+    emptyCheck: function (value) {
+      let flag = false;
+
+      value.settings.forEach((item) => {
+        if (item.source == 'none') flag =true;
+        if (item.destination == 'none') flag =true;
+      });
+      return flag;
+    },
+
+    /***********************************************************
+     * [重複チェック処理関数]
+     * @param {array} value [保存設定内容]
+     * @returns [重複している場合 true　していない場合 falseを返す]
+     **********************************************************/
+    dupCheck: function (value) {
+      const array = [];
+
+      value.settings.forEach((item) => {
+        array.push(item.destination);
+      });
+      const a = new Set(array);
+      return a.size !== array.length;
+    },
+
+    /***********************************************************
+     * [テーブル跨ぎチェック処理関数]
+     * @param {array} value [テーブル跨ぎチェックを行う配列]
+     * @returns [跨いでいる場合 true　いない場合 falseを返す]
+     **********************************************************/
+    tableCheck: function (value) {
+      let flag = false;
+
+      value.settings.forEach((item) => {
+        if (obj.getTableName(item.source) !== obj.getTableName(item.destination)) flag = true;
+      });
+      return flag;
+    },
+
+    /***********************************************************
+     * [テーブル名抽出関数]
+     * @param {array} value [フィールド名]
+     * @returns [テーブル内の場合テーブル名　テーブル外の場合空文字列を返す]
+     **********************************************************/
+    getTableName: function(fieldName){
+      const parts = fieldName.split('　');
+      if (parts.length !== 2) return '';
+      const parent = obj.fieldList.find((x) => x.code === parts[0]);
+      if (parent.type === 'SUBTABLE') return parts[0];
+      return '';
+    },
+
+    /************************************************
+     * [保存・キャンセルボタンクリック時イベント処理関数]
+     ***********************************************/
+    buttonClickEvent: function () {
+      //[保存ボタンクリック時イベント]
+      obj.$submit.on('click', function (e) {
+        e.preventDefault();
+
+        const value = obj.submit();
+        if (obj.emptyCheck(value)) {
+          obj.displayAlert('エラー', '反映元または反映先フィールドが選択されていません。', 'error', 'OK');
+          return false;
+        }
+
+        if (obj.dupCheck(value)) {
+          obj.displayAlert('エラー', '選択された反映先フィールドが重複しています。', 'error', 'OK');
+          return false;
+        }
+
+        if (obj.tableCheck(value)) {
+          obj.displayAlert('エラー', '別テーブルに属するフィールドは選択できません。', 'error', 'OK');
+          return false;
+        }
+        //[文字列に変換]
+        value.settings = JSON.stringify(value.settings);
+
+        kintone.plugin.app.setConfig(value);
+      });
+
+      //[キャンセルボタンクリック時イベント]
+      obj.$cancel.on('click', function () {
+        window.location.href = '../../' + kintone.app.getId() + '/plugin/';
+      });
+    },
+
+    checkCertificationFile: function () {
+      if (typeof KNTP283810certification === 'function') {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  };
+
+  //[関数実行]
+  obj.configShow(obj.config);
+})(jQuery, kintone.$PLUGIN_ID);
---
+++ plugins/UserSelectStringAssignment/UserSelectStringAssignment/contents/js/mobile.js
@@ -0,0 +1,238 @@
+// Copyright (C) All in one Allright Reserved.
+
+jQuery.noConflict();
+
+(async function ($, PLUGIN_ID) {
+  'use strict';
+
+  if (!(await KNTP283810certification())) {
+    return;
+  }
+
+  const config = kintone.plugin.app.getConfig(PLUGIN_ID);
+
+  try {
+    if (Object.keys(config).length) {
+      config.settings = JSON.parse(config.settings);
+    }
+    // console.log(config);
+  } catch (ignore) { }
+
+  const obj = {
+    events1: {},
+    events2: {},
+    func: {},
+  };
+
+  obj.events1.type = [];
+  if (config.settings && config.settings.length) {
+    for (const setting of config.settings) {
+      const s = setting.source.split('　');
+      const field = s.length > 1 ? s[1] : s[0];
+      obj.events1.type.push('mobile.app.record.edit.change.' + field);
+      obj.events1.type.push('mobile.app.record.create.change.' + field);
+      obj.events1.type.push('mobile.app.record.index.edit.change.' + field);
+    }
+  }
+
+  obj.events2.type = ['mobile.app.record.index.show'];
+
+  obj.events1.handler = function (e) {
+    // if (!Object.keys(config).length) return e;
+    // obj.func.jsonParse();
+
+    for (const setting of config.settings) {
+      // フィールド名の分割
+      const sourceSplit = setting.source.split('　');
+      const sourceName = sourceSplit.length === 1 ? sourceSplit[0] : sourceSplit[1];
+      const destinationSplit = setting.destination.split('　');
+      const destinationName = destinationSplit.length === 1 ? destinationSplit[0] : destinationSplit[1];
+      let tableCode = '';
+      if (destinationSplit.length !== 1) {
+        tableCode = destinationSplit[0];
+        if (!e.record[tableCode]) tableCode = '';
+      }
+
+      //[フィールド存在チェック]
+      if (obj.func.existenceCheck(tableCode, sourceName, destinationName, e)) continue;
+
+      //[テーブル外フィールド]
+      if (tableCode == '') {
+        const sourceField = e.record[sourceName];
+        const destinationField = e.record[destinationName];
+
+        if (sourceField.value.length) {
+          const sourceFieldValueMap = sourceField.value.map((x) => x['name']);
+          destinationField.value = sourceFieldValueMap.join(setting.delimiter);
+        }
+
+      //[テーブル内フィールド]
+      } else {
+        for (const row of e.record[tableCode].value) {
+          const sourceField = row.value[sourceName];
+          const destinationField = row.value[destinationName];
+
+          if (sourceField.value.length) {
+            const sourceFieldValueMap = sourceField.value.map((x) => x['name']);
+            destinationField.value = sourceFieldValueMap.join(setting.delimiter);
+          }
+        }
+      }
+    }
+    return e;
+  };
+
+  obj.events2.handler = async function (e) {
+    if (!config.settings || !config.settings.length) return e;
+
+    const fieldList = await obj.func.getFieldList();
+    if (!fieldList || fieldList.length === 0) return e;
+
+    const missingFields = [];
+
+    for (const setting of config.settings) {
+      if (setting.destination && setting.destination !== 'none') {
+        const exists = fieldList.some(field => field.code === setting.destination);
+        if (!exists) {
+          missingFields.push(`[反映先フィールド] ${setting.destination}`);
+        }
+      }
+
+      if (setting.source && setting.source !== 'none') {
+        const exists = fieldList.some(field => field.code === setting.source);
+        if (!exists) {
+          missingFields.push(`[反映元フィールド] ${setting.source}`);
+        }
+      }
+    }
+
+    const uniqueMissingFields = [...new Set(missingFields)];
+
+    if (uniqueMissingFields.length > 0) {
+      const imageUrl = 'https://allin-one.cybozu.com/k/api/record/download.do/-/%E3%82%A4%E3%83%B3%E3%83%95%E3%82%A9.png?app=4215&thumbnail=true&field=6630014&detectType=true&record=6&row=1613353&id=247948&hash=f1024070e9eab225a306f666ea7b1c567b4bb325&revision=1&.png&w=150&h=150&flag=SHRINK';
+      const fieldHtml = uniqueMissingFields
+        .map(code => `・${code}`)
+        .join('<br>');
+
+      obj.func.displayAlert(
+        '警告',
+        '「ユーザー選択→文字列1行代入プラグイン」に設定済みのフィールドコードが変更または削除されています。<br><br>' +
+        '対象フィールドコード：<br>' +
+        fieldHtml +
+        '<br><br>プラグイン設定を修正してください。',
+        imageUrl,
+        'OK'
+      );
+    }
+
+    return e;
+  };
+
+  // obj.func.jsonParse = function () {
+  //   try {
+  //     config.settings = JSON.parse(config.settings);
+  //     // console.log(config);
+  //   } catch (ignore) { }
+  // };
+
+  /**
+   * [指定したフィールドが存在するか確認]
+   * @param {string} tableCode [テーブル名]
+   * @param {string} source  [ユーザー選択名]
+   * @param {string} destination  [文字列名]
+   * @param {object} record  [イベント実行時のフォーム情報]
+   * @returns [存在しない場合true 存在する場合false]
+   */
+  obj.func.existenceCheck = function (tableCode, source, destination, { record }) {
+    if (!tableCode) {
+      const field = record[destination];
+      const field2 = record[source];
+      if (!field || !field2) return true;
+      return false;
+    } else {
+      const tableField = record[tableCode];
+      if (!tableField) return true;
+      const field = tableField.value[0].value[destination];
+      const field2 = tableField.value[0].value[source];
+      if (!field || !field2) return true;
+      return false;
+    }
+  };
+
+  obj.func.getFieldList = async function () {
+    const fieldList = [];
+    const fieldList2 = [
+      ...Object.values(cybozu.data.page.FORM_DATA.schema.table.fieldList),
+      ...Object.values(cybozu.data.page.FORM_DATA.schema.subTable),
+    ];
+
+    try {
+      const resp = await kintone.api(kintone.api.url('/k/v1/app/form/layout.json', true), 'GET', {
+        app: kintone.mobile.app.getId(),
+      });
+      resp.layout.forEach((row) => {
+        if (row.type === 'ROW') row.fields.forEach((field) => fieldList.push(field));
+        else if (row.type === 'SUBTABLE') {
+          fieldList.push(row);
+          //if (!subTable) return;
+          row.fields.forEach((field) => {
+            const fieldInfo = {
+              type: field.type,
+              code: `${row.code}　${field.code}`,
+              id: `${fieldList2.find((x) => x.var === row.code).id}　${Object.values(fieldList2.find((x) => x.var === row.code).fieldList).find((y) => y.var === field.code).id
+                }`,
+              properties: Object.values(fieldList2.find((x) => x.var === row.code).fieldList).find(
+                (y) => y.var === field.code
+              ).properties,
+            };
+            fieldList.push(fieldInfo);
+          });
+        } else if (row.type === 'GROUP') {
+          fieldList.push(row);
+          row.layout.forEach((childRow) => childRow.fields.forEach((field) => {
+            fieldList.push({
+              type: field.type,
+              code: row.code + '　' + field.code,
+              id: field.id,
+              properties: field.properties,
+            })
+          }
+            //fieldList.push(field)
+          ));
+        }
+      });
+    } catch { }
+
+    fieldList.forEach((field) => {
+      const target = fieldList2.find((x) => x.var === field.code);
+      if (!target) return;
+      field.id = target.id;
+      field.properties = target.properties;
+      field.label = target.label;
+
+      if (!(field.type === 'SUBTABLE' && field.id === target.id)) return;
+      field.fields.forEach((inField) => {
+        const inTarget = Object.values(target.fieldList).find((x) => x.var === inField.code);
+        inField.id = inTarget.id;
+        inField.properties = inTarget.properties;
+        inField.label = inTarget.label;
+      });
+    });
+    return fieldList;
+  };
+
+  obj.func.displayAlert = function (title, text, type, button) {
+    swal.fire({
+      title: title,
+      html: text,
+      imageUrl: type,
+      confirmButtonText: button,
+      customClass: {
+        popup: 'my-popup-class',
+      }
+    });
+  };
+
+  kintone.events.on(obj.events1.type, obj.events1.handler);
+  kintone.events.on(obj.events2.type, obj.events2.handler);
+})(jQuery, kintone.$PLUGIN_ID);
